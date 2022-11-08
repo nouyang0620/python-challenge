d0d971ff33361f39472dd93e0b1987b1f3584887
Nou Yang <nouyang0620@gmail.com>
2022-11-07 23:12:56 -0600

    PyPoll progress

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -23,6 +23,7 @@ with open(elect_csv, encoding='utf') as csvfile:
     names = []
     vote_percentage = []
     vote_count = []
+    winner_count = 0
 
     # read each row of data after the header
     for row in csvreader:
@@ -47,13 +48,37 @@ with open(elect_csv, encoding='utf') as csvfile:
         y = (x/total_votes)*100
         vote_percentage.append(y)
 
+    # get the highest vote count from list
     winner_count = max(vote_count)
-    winner = name[vote_count.index(winner_count)]
+    # get the winner name based on the winning count
+    winner = names[vote_count.index(winner_count)]
 
 # output results
-print("Election Results")
+print("\nElection Results")
 print("-------------------------")
 print("Total Votes: " + str(total_votes))
 print("-------------------------")
+# print the name, vote percentage, and vote count for each candidate
+for i in range(len(names)):
+    print(names[i] + ": " + str(round(vote_percentage[i], 3)) + "% (" + str(vote_count[i]) + ")")
+print("-------------------------")
+print("Winner: " + str(winner))
+print("-------------------------")
 
+# set path for text file
+poll_analysis = os.path.join('analysis','poll_analysis.txt')
+
+# open and write results to text file
+with open(poll_analysis,"w") as text:
+    text.write("Election Results\n")
+    text.write("-------------------------\n")
+    text.write("Total Votes: " + str(total_votes))
+    text.write("\n-------------------------\n")
+    # write the name, vote percentage, and vote count for each candidate
+    for i in range(len(names)):
+        text.write(names[i] + ": " + str(round(vote_percentage[i], 3)) + "% (" + str(vote_count[i]) + ")\n")
+    text.write("-------------------------\n")
+    text.write("Winner: " + str(winner))
+    text.write("\n-------------------------")
+    
 
